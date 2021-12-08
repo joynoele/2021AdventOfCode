1e9c9977ce10b1ba3d7c0a588b2cbbf98eb3c05a
Elsa Vezino <Elsa.Vezino@ivanti.com>
2021-12-08 15:41:46 -0600

    initial checkin again

---
+++ Day01/SonarSweep/SonarSweep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SonarSweep
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var input = @"C:\Projects\Repos\ElsaVezino\2021AdventOfCode\Day01\SonarSweep\SonarSweep\Input.txt";
+            foreach (string reading in System.IO.File.ReadLines(input))
+            {
+                Console.WriteLine($"{reading}");
+            }
+        }
+    }
+}
